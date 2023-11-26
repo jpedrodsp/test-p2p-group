8d75347a731eb26edb563a97806c1f9f7fadb75f
Jhonnatas Ribeiro <97709889+JhonnatasRibeiroDev@users.noreply.github.com>
2023-11-26 14:33:42 -0300

    program incomplete
    The program requires the addition of new features for use

---
+++ p2p.py
@@ -0,0 +1,1 @@
+This project aims to create a Peer-to-Peer (P2P) application in Python using sockets for the Computer Networks discipline. The application will allow connection between at least 5 devices, facilitating the exchange of files and checking the availability of desired files on the network.
