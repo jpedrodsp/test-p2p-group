360792f4b144a5764be2c3c8e46695c910fa5de0
João Pedro Patrocinio <jpedro9966@gmail.com>
2023-12-03 11:35:36 -0300

    Initial boilerplate preparation

--- p2p.py
+++ p2p.py
@@ -1,1 +1,6 @@
+"""
 This project aims to create a Peer-to-Peer (P2P) application in Python using sockets for the Computer Networks discipline. The application will allow connection between at least 5 devices, facilitating the exchange of files and checking the availability of desired files on the network.
+"""
+
+if __name__ == '__main__':
+    pass
