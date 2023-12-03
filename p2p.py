4da629401caf834e06bab11704229ed4f7a7428b
João Pedro Patrocinio <jpedro9966@gmail.com>
2023-12-03 11:43:02 -0300

    Add class 'Application' to entrypoint

--- p2p.py
+++ p2p.py
@@ -2,5 +2,12 @@
 This project aims to create a Peer-to-Peer (P2P) application in Python using sockets for the Computer Networks discipline. The application will allow connection between at least 5 devices, facilitating the exchange of files and checking the availability of desired files on the network.
 """
 
+class Application:
+    def __init__(self) -> None:
+        self.peers = {}
+    def run(self):
+        pass
+
 if __name__ == '__main__':
-    pass
+    app = Application()
+    app.run()
